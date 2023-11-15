e5da4a1d18a8b708bbc71d51ea45837672ac3cae
Amy <ampyaephyonaing@gmail.com>
2023-11-15 21:27:49 +0800

    Creating & extending Custom Errors interface

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,28 +1,18 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-
+ 
+import { CustomError } from "../errors/custom-errors";
+CustomError
 export const errorHandler = (
     err: Error, 
     req: Request, 
     res: Response, 
     next: NextFunction
 ) => { 
-    if(err instanceof RequestValidationError){
-        const formattedErrors = err.errors.map(error => {
-           
-            return { message: error.msg};
-        });
-
-        return res.status(400).send({ errors:formattedErrors });
-    }
-
-
-    if(err instanceof DatabaseConnectionError){
-        return res.status(500).send({ errors: [{ message : err.reason }]})
-    }
+    if(err instanceof CustomError){ 
+        return res.status(err.errorCode).send({ errors: err.serializeErrors() });
+    } 
 
     res.status(400).send({
-        errors :[{  message : err.message }]
+        errors :[{  message :  'Something went wrong' }]
     });
 };
