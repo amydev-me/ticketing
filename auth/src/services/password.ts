2e230ea982ea7615bb706cc86e0b9c034ad07218
Amy <ampyaephyonaing@gmail.com>
2023-11-16 21:42:19 +0800

    Creating Password - Hashing Service

---
+++ auth/src/services/password.ts
@@ -0,0 +1,21 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const sryptAsync = promisify(scrypt);
+
+export class Password {
+    static async toHash(password: string){
+        const salt = randomBytes(8).toString('hex');
+        const buf = (await sryptAsync(password, salt, 64)) as Buffer;
+
+        return `${buf.toString('hex')}.${salt}`
+    }
+
+    static compare(storedPassword:string, suppliedPassword: string){
+
+    }
+
+    print() {
+
+    }
+}
