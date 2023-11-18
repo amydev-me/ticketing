8b3f59f34133edf86d074657256b6f6c93e75e8e
Amy <ampyaephyonaing@gmail.com>
2023-11-18 23:18:32 +0800

    Generating a JWT Token

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
+router.post('/api/users/signup', [ 
     body('email')
         .isEmail()
         .withMessage('Email must be valid'),
@@ -34,6 +35,15 @@ async (req: Request, res: Response) => {
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign({ 
+        id: user.id,
+        email: user.email
+    }, 'mykey');
+ 
+    req.session = {
+        jwt : userJwt
+    };
 
     return res.status(201).send(user);
 })
