64c6166c4d32118718912e75b3d69db973c49958
Amy <ampyaephyonaing@gmail.com>
2023-11-19 13:50:52 +0800

    Testing Invalid Input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -3,10 +3,46 @@ import { app } from '../../app';
 
 it('returns a 201 on successful signup', async () => {
     return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
+                .post('/api/users/signup')
+                .send({
+                    email: 'test@test.com',
+                    password: 'password'
+                })
+                .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+                .post('/api/users/signup')
+                .send({
+                    email: 'testest.com',
+                    password: 'password'
+                })
+                .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+                .post('/api/users/signup')
+                .send({
+                    email: 'tes@test.com',
+                    password: 'p'
+                })
+                .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app)
+                .post('/api/users/signup')
+                .send({
+                    email: 'test@test.com'
+                })
+                .expect(400);
+
+    await request(app)
+                .post('/api/users/signup')
+                .send({
+                    password:'password'
+                })
+                .expect(400);
 });
