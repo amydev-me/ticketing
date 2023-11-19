623f0e805d450e2d04b21beb55aa1e1fb55fca72
Amy <ampyaephyonaing@gmail.com>
2023-11-19 09:34:53 +0800

    Middleware - Request Validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
-
+import { body } from 'express-validator';
+import { validationRequest } from '../middlewares/validate-requests';
+ 
 const router = express.Router();
 
 router.post('/api/users/signin', [
@@ -12,16 +12,11 @@ router.post('/api/users/signin', [
         .trim()
         .notEmpty()
         .withMessage('You must supply a password')
-], (req: Request, res: Response) => {
-
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
-    }
-    
+], 
+validationRequest, 
+(req: Request, res: Response) => {
     res.send('Hi there!');
-})
+});
 
 
 export { router as signinRouter };
