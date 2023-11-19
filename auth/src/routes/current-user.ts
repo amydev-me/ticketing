7a8996a29dc80e450d0973b38f513b65ddf3481e
Amy <ampyaephyonaing@gmail.com>
2023-11-19 09:58:26 +0800

    Returning Current User

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,22 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+router.get('/api/users/currentuser', (req: Request, res: Response) => { 
+    // equivalent !req.session || !req.session.jwt = req.session?.jwt
+    if(!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
 
-router.get('/api/users/currentuser', (req, res) => { 
-    res.send('Hi there!');
-}) 
+    try{
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+        res.send({ currentUser: payload });
+    }
+    catch (err) {
+        res.send({ currentUser: null });
+    } 
+});
 
 export { router as currentUserRouter };
