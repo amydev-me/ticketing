91a00719c06db3baeffb60d3e5bd89803e978cac
Amy <ampyaephyonaing@gmail.com>
2023-11-28 22:33:21 +0800

    Creating the router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,8 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@amytickets/common';
 
+import { createTicketRouter } from './routes/new';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -14,6 +16,8 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 ); 
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
