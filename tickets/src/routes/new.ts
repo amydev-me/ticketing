9a97455626f9c7fd27579817fe7d2f56671e6f07
Amy <ampyaephyonaing@gmail.com>
2023-11-29 21:23:16 +0800

    Validate Title & Price and Testing

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@amytickets/common';
+import { body } from 'express-validator';
+import { requireAuth,validationRequest } from '@amytickets/common';
 
 const router = express.Router();
 
 
-router.post('/api/tickets', requireAuth, (req:Request, res:Response) => {
+router.post('/api/tickets', requireAuth,[
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0.')
+], validationRequest, (req:Request, res:Response) => {
+    
     res.sendStatus(200);
 });
 
