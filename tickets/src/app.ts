1e1759abb82735183ec5693f451f49f77155fdde
Amy <ampyaephyonaing@gmail.com>
2023-11-29 22:07:43 +0800

    Express Use ShowRouter

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,7 +6,7 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@amytickets/common';
 
 import { createTicketRouter } from './routes/new';
-
+import { showTicketRouter } from './routes/show';
 const app = express();
 app.set('trust prox y', true);
 app.use(json());
@@ -18,6 +18,7 @@ app.use(
 ); 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
