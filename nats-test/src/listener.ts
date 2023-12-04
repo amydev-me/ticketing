d096aa2ced3774c3719113ee8c2e9a2066cc6e28
Amy <ampyaephyonaing@gmail.com>
2023-12-04 23:22:30 +0800

    Nats Streaming Server Testing

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,23 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+
+console.clear();
+
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222'
+});
+
+client.on('connect', () => {
+    console.log('Publisher connected to NATS');
+
+    const subscription = client.subscribe('ticket:created', 'order-service-queue-group');
+
+    subscription.on('message', (msg: Message) => {
+        const data =  msg.getData();
+
+        if ( typeof data === 'string'){
+            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+        }
+     });
+});
