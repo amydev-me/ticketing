f1b17d4278ce6f5852c6be7450745f6823f88c1d
Amy <ampyaephyonaing@gmail.com>
2023-12-10 16:46:16 +0800

    Updating & Publishing Ticket Update

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validationRequest, NotFoundError, NotAuthorizedError } from '@amytickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,6 +25,13 @@ router.put('/api/tickets/:id', requireAuth,[
 
     ticket.set({ title, price });
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title,
+        price,
+        userId : ticket.userId
+    })
     
     res.send(ticket);
 });
