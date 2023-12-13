f0b9c7d5d3e059c05a125320c04102148e9315f5
Amy <ampyaephyonaing@gmail.com>
2023-12-13 22:45:47 +0800

    Publishing Order Creation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import { requireAuth, validationRequest, NotFoundError, BadRequestError } from '
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -47,6 +49,17 @@ router.post('/api/orders',
         });
 
         await order.save();
+
+        new OrderCreatedPublisher(natsWrapper.client).publish({
+            id: order.id,
+            status: order.status,
+            userId: order.userId,
+            expiresAt: order.expiresAt.toISOString(),
+            ticket: {
+                id: ticket.id,
+                price: ticket.price
+            }
+        });
 
         res.status(201).send(order);
     }   
