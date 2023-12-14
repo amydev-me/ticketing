71d43003e9a60a27921fe8e81e108c7632d01d87
Amy <ampyaephyonaing@gmail.com>
2023-12-14 15:00:54 +0800

    Add ticket id in listener

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -9,9 +9,11 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     queueGroupName = queueGroupName;
 
     async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-        const { title, price } = data;
+        const { id, title, price } = data;
         const ticket = Ticket.build({
-            title, price
+            id, 
+            title, 
+            price
         });
 
         await ticket.save();
