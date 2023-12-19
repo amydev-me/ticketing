9337b1262c4bccc79f21033189bb24858de4e4f5
Amy <ampyaephyonaing@gmail.com>
2023-12-19 17:25:43 +0800

    Add version in order

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { OrderStatus } from "@amytickets/common";
 import { TicketDoc } from "./ticket";
-
 export { OrderStatus };
 
 // An interface that describes the properties
@@ -57,6 +57,9 @@ const orderSchema = new mongoose.Schema({
         }
     }
 }); 
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
     return new Order(attrs);
