774726ce79afe4bc39d475960d8397b8b031efa5
Amy <ampyaephyonaing@gmail.com>
2023-12-23 22:54:10 +0800

    Creating a Charge with Stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validationRequest, NotFoundError, NotAuthorizedError, OrderStatus, BadRequestError } from '@amytickets/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -25,7 +26,11 @@ router.post('/api/payments', requireAuth, [
         throw new BadRequestError('Cannot pay for an cancelled order.');
     }
     
-    
+    await stripe.charges.create({
+        currency: 'usd',
+        amount: order.price * 100,
+        source: token
+    })
 
     res.send({ success: true });
 });
