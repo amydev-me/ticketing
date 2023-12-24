8b442e0b5e7a85d79333ca3bf5737dffbaf10576
Amy <ampyaephyonaing@gmail.com>
2023-12-24 22:00:43 +0800

    Paying for an order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,46 +1,49 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import userRequest from '../../hooks/user-request';
+import useRequest from '../../hooks/user-request';
 import Router from 'next/router';
 
 const OrderShow = ({ order, currentUser }) => {
-    const [timeLeft, setTimeLeft] = useState('');
-    const { doRequest, errors } = userRequest({
+    const [timeLeft, setTimeLeft] = useState(0);
+    const { doRequest, errors } = useRequest({
         url: '/api/payments',
         method: 'post',
         body: {
-            orderId: order.id
+            orderId: order.id,
         },
-        onSuccess: (payment) = Router.push('/orders')
-    })
+        onSuccess: () => Router.push('/orders'),
+    });
+
     useEffect(() => {
         const findTimeLeft = () => {
             const msLeft = new Date(order.expiresAt) - new Date();
             setTimeLeft(Math.round(msLeft / 1000));
-        }
+        };
 
         findTimeLeft();
         const timerId = setInterval(findTimeLeft, 1000);
 
         return () => {
             clearInterval(timerId);
-        }
-    }, [order])
+        };                          
+    }, [order]);
+    console.log(order)
 
-    if(timeLeft < 0){
-        return <div>Order Expired</div>
+    if (timeLeft < 0) {
+        return <div>Order Expired</div>;
     }
 
 
     return (
         <div>
             Time left to pay: {timeLeft} seconds
-            <StripeCheckout 
-                token={({ id }) => doRequest({token: id})}
-                stripeKey='STRIPE_KEY'
+            
+            <StripeCheckout
+                token={({ id }) => doRequest({ token: id })}
+                stripeKey='pk_test_51OQW86G7z7yHZmvo7dXIilcsqzIUZcOu9o2uUmg9ciUFuqRH5aDTwUfCzo6e9aOQhP0Cp91YcpGrYUPHTXrxHaN500Cl8eGXwD'
                 amount={order.ticket.price * 100}
                 email={currentUser.email}
-            />
+            ></StripeCheckout>
             {errors}
         </div>
     );
@@ -49,8 +52,8 @@ const OrderShow = ({ order, currentUser }) => {
 OrderShow.getInitialProps = async (context, client) => {
     const { orderId } = context.query;
     const { data } = await client.get(`/api/orders/${orderId}`);
-
+  
     return { order: data };
-}
-
+};
+  
 export default OrderShow;
