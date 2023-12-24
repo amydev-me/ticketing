53c87cf4cb37dd6d4972f1e08912b161b66b52d4
Amy <ampyaephyonaing@gmail.com>
2023-12-24 20:37:46 +0800

    Show Order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -37,7 +37,7 @@ const OrderShow = ({ order, currentUser }) => {
             Time left to pay: {timeLeft} seconds
             <StripeCheckout 
                 token={({ id }) => doRequest({token: id})}
-                stripeKey='pk_test_51OQW86G7z7yHZmvo7dXIilcsqzIUZcOu9o2uUmg9ciUFuqRH5aDTwUfCzo6e9aOQhP0Cp91YcpGrYUPHTXrxHaN500Cl8eGXwD'
+                stripeKey='STRIPE_KEY'
                 amount={order.ticket.price * 100}
                 email={currentUser.email}
             />
